2798f78b32b38d32697e2d0843a9af2e718ff09e
Kyle Spier-Swenson <kyleshome@gmail.com>
2022-05-30 00:16:19 -0700

    Removes stupidity
    This line was never legally enforceable but I was too college aged to understand that at the time.

--- byond-cdn/rscfilelist.cpp
+++ byond-cdn/rscfilelist.cpp
@@ -33,7 +33,6 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  THE RESULTING OUTPUT IS COVERED UNDER THIS SAME LICENSE AS THE COMBINED WORK OF THE USER AND THE AUTHOR.
  */
 
 // istream::ignore example
